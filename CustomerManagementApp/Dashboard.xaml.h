4dc14df7e48d33be81a14edcb8bc69048d41c3f1
Adebayo Salami <salamibolarinwa16@gmail.com>
2021-10-13 23:46:55 +0100

    initial commit

---
+++ CustomerManagementApp/Dashboard.xaml.h
@@ -0,0 +1,21 @@
+﻿//
+// Dashboard.xaml.h
+// Declaration of the Dashboard class
+//
+
+#pragma once
+
+#include "Dashboard.g.h"
+
+namespace CustomerManagementApp
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	[Windows::Foundation::Metadata::WebHostHidden]
+	public ref class Dashboard sealed
+	{
+	public:
+		Dashboard();
+	};
+}
---
+++ CustomerManagementApp/MainPage.xaml.cpp
@@ -0,0 +1,34 @@
+﻿//
+// MainPage.xaml.cpp
+// Implementation of the MainPage class.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+using namespace Windows::UI::Popups;
+using namespace CustomerManagementApp;
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+}
+
+
+void CustomerManagementApp::MainPage::Btn_AddCustomer(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	MessageDialog mesageDialogue("C++ -- Customer Added Succesfully!");
+	mesageDialogue.ShowAsync();
+}
